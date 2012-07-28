85b4b3601caf388c582c6a7eb9dd94f82efdf14d
lukasz.dzianach.iqube <lukasz.dzianach.iqube@675ba97f-8685-4f27-b61a-676bf16e0098>
2012-07-28 21:48:23 +0000

    Changes in license.

--- bench/tb_salsa20.vhd
+++ bench/tb_salsa20.vhd
@@ -1,17 +1,24 @@
+--
+-- Copyright 2012 iQUBE research
+--
 -- This file is part of Salsa20IpCore.
-
+--
 -- Salsa20IpCore is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Lesser General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
-
+--
 -- Salsa20IpCore is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU Lesser General Public License for more details.
-
+--
 -- You should have received a copy of the GNU Lesser General Public License
 -- along with Salsa20IpCore.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- contact: Rúa Fonte das Abelleiras s/n, Campus Universitario de Vigo, 36310, Vigo (Spain)
+-- e-mail: lukasz.dzianach@iqube.es, info@iqube.es
+-- 
 
 LIBRARY ieee  ; 
 LIBRARY std  ; 
